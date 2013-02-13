83a563d32918d44133839e4005787cb2b3b2f562
Tim Otten <to-git@think.hm>
2013-02-12 21:59:36 -0500

    Remove debug code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,3 @@
 <?php
-printf("require [%s]\n", dirname(__FILE__) . '/../vendor/autoload.php');
 $loader = require_once dirname(__FILE__) . '/../vendor/autoload.php';
 $loader->add('CA_Config', __DIR__);
