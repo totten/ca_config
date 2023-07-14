d5ae85f37bef97b9338677e21b47e0aeb3ecbf78
Seamus Lee <seamuslee001@gmail.com>
2023-07-14 12:21:42 +1000

    [REF] Update PHPunit Test to work with phpunit8

--- tests/CA/Config/TestBase.php
+++ tests/CA/Config/TestBase.php
@@ -3,7 +3,7 @@
 /**
  * Test the probe() produces a properly functioning SSL configuration
  */
-abstract class CA_Config_TestBase extends PHPUnit_Framework_TestCase {
+abstract class CA_Config_TestBase extends PHPUnit\Framework\TestCase {
     const VALID_SSL_URL = 'https://drupal.org/INSTALL.mysql.txt';
     const INVALID_SSL_URL = 'https://www-test.civicrm.org:4433/index.html';
 
@@ -24,14 +24,14 @@ abstract class CA_Config_TestBase extends PHPUnit_Framework_TestCase {
      */
     abstract function get($url, $caConfig);
 
-    public function testValidUrl_Default() {
+    public function testValidUrl_Default(): void {
         $caConfig = $this->probe();
         $this->assertTrue($caConfig->isEnableSSL(), 'SSL should be enabled');
         $response = $this->get(self::VALID_SSL_URL, $caConfig);
         $this->assertNotEmpty($response, sprintf('Valid HTTPS URL (%s) should produce valid response data (Policy: %s)', self::VALID_SSL_URL, var_export($caConfig, TRUE)));
     }
 
-    public function testValidUrl_noVerify() {
+    public function testValidUrl_noVerify(): void {
         $caConfig = $this->probe(array(
             'verify_peer' => FALSE,
         ));
@@ -40,14 +40,14 @@ abstract class CA_Config_TestBase extends PHPUnit_Framework_TestCase {
         $this->assertNotEmpty($response, sprintf('Valid HTTPS URL (%s) without verification should produce valid response data (Policy: %s)', self::VALID_SSL_URL, var_export($caConfig, TRUE)));
     }
 
-    public function testInvalidUrl_Default() {
+    public function testInvalidUrl_Default(): void {
         $caConfig = $this->probe();
         $this->assertTrue($caConfig->isEnableSSL(), 'SSL should be enabled');
         $response = $this->get(self::INVALID_SSL_URL, $caConfig);
         $this->assertEmpty($response, sprintf('Invalid HTTPS URL (%s) should produce invalid response data (Policy: %s)', self::INVALID_SSL_URL, var_export($caConfig, TRUE)));
     }
 
-    public function testInvalidUrl_noVerify() {
+    public function testInvalidUrl_noVerify(): void {
         $caConfig = $this->probe(array(
             'verify_peer' => FALSE,
         ));
